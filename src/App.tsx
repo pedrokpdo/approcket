9d74368a30108a1a1a3b7ed68d3b6d7928a8687f
pedrokpdo <100696926+pedrokpdo@users.noreply.github.com>
2022-03-28 16:17:08 -0300

    introduçao ao contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./styles/global"
 import Modal from 'react-modal'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 Modal.setAppElement('#root')
 
@@ -16,15 +17,15 @@ export function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <NewTransactionModal 
-      isOpen={isNewTransactionModalOpen}
-      onRequestClose={handleCloseNewTransactionModal}
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
       />
-        <GlobalStyle />
-    </>
+      <GlobalStyle />
+    </TransactionsContext.Provider>
   );
 }
 
